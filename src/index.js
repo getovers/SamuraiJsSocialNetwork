cb3deff1285e65b9480eec2f3cc5fb8cc9875b29
Vlad Khotynskiy <vlhotinskiy@gmail.com>
2023-06-30 18:21:26 +0300

    determine store and merge there state and functions

--- src/index.js
+++ src/index.js
@@ -3,21 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { addPost, subscribe, updatePostText } from './redux/state';
-import state from './redux/state';
+import store from './redux/state';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 let rerenderEntireTree = (state) => {
   root.render(
     <React.StrictMode>
-      <App state={state} addPost={addPost} updatePostText={updatePostText} />
+      <App state={state} addPost={store.addPost.bind(store)} updatePostText={store.updatePostText.bind(store)} />
     </React.StrictMode>
   );
 }
 
-subscribe(rerenderEntireTree);
-rerenderEntireTree(state)
+store.subscribe(rerenderEntireTree);
+rerenderEntireTree(store.state)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
