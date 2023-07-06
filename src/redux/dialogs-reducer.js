17d01eef85427ea53cfc9afa6372a6c6eca0ece8
Vlad Khotynskiy <vlhotinskiy@gmail.com>
2023-07-06 17:18:15 +0300

    refactored code by '...' operator in profile, dialogs reducers; now it is possible to send posts and messages

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -9,13 +9,13 @@ let initialState = {
         { id: 4, name: "Igor" },
         { id: 5, name: "Misha" },
         { id: 6, name: "Katya" }
-      ],
-      messages: [
+    ],
+    messages: [
         { id: 1, message: "Hello" },
         { id: 2, message: "How are you?" },
         { id: 3, message: "I'm fine thanks" },
-      ],
-      newMessageText: ''
+    ],
+    newMessageText: ''
 }
 
 const dialogsReducer = (state = initialState, action) => {
@@ -23,17 +23,17 @@ const dialogsReducer = (state = initialState, action) => {
         case SEND_MESSAGE:
             let newMessageText = state.newMessageText;
             if (newMessageText) {
-                let newMessage = {
-                    id: 4,
-                    message: newMessageText
-                }
-                state.messages.push(newMessage)
-                state.newMessageText = ''
+                return {
+                    ...state,
+                    newMessageText: '',
+                    messages: [...state.messages, { id: 4, message: newMessageText }]
+                };
             }
-            return state;
         case UPDATE_MESSAGE_TEXT:
-            state.newMessageText = action.newText;
-            return state;
+            return {
+                ...state,
+                newMessageText: action.newText
+            };
         default:
             return state;
     }
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -12,25 +12,24 @@ let initialState = {
 
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_POST: {
+        case ADD_POST:
             if (state.newPostText) {
                 let newPost = {
                     id: 4,
                     message: state.newPostText,
                     likesCount: 0
                 };
-                let stateCopy = { ...state }
-                stateCopy.posts = [...state.posts]
-                stateCopy.posts.push(newPost);
-                stateCopy.newPostText = '';
-                return stateCopy;
+                return {
+                    ...state,
+                    posts: [...state.posts, newPost],
+                    newPostText: ''
+                }
             }
-        }
-        case UPDATE_POST_TEXT: {
-            let stateCopy = { ...state }
-            stateCopy.newPostText = action.newText;
-            return stateCopy;
-        }
+        case UPDATE_POST_TEXT:
+            return {
+                ...state,
+                newPostText: action.newText
+            }
         default:
             return state;
     }
