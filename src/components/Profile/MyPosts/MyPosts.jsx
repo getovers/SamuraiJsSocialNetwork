b8feeed68085435bbdd3cd64885c6cb440018641
Vlad Khotynskiy <vlhotinskiy@gmail.com>
2023-07-06 17:28:09 +0300

    added a key prop for components(elements) created by map method

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,7 +3,7 @@ import styles from './MyPosts.module.css'
 import Post from './Post/Post';
 
 function MyPosts(props) {
-    let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount} />)
+    let postsElements = props.posts.map(p => <Post message={p.message} key={p.id} likesCount={p.likesCount} />)
 
     let onAddPost = () => {
         props.addPost();
