ece7783203d4b57eaf9ebe2d32df5b704c5f128a
Vlad Khotynskiy <vlhotinskiy@gmail.com>
2023-07-11 17:53:52 +0300

    downloaded axios library, and now it is possible to send get query and get data in functional component Users.jsx

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,27 +1,39 @@
 import styles from './Users.module.css'
+import axios from 'axios'
+import userPhoto from '../../photos/userPhoto.png'
 
 function Users(props) {
-    if(props.users.length === 0){
-        props.setUsers([
-            {id: 1, photoUrl: 'https://img.freepik.com/free-icon/user_318-159711.jpg', followed: true, fullName: "Vlad K.", status: "i am ukrainian", location:{country: 'Ukraine', city: 'Lutsk'}},
-            {id: 2, photoUrl: 'https://img.freepik.com/free-icon/user_318-159711.jpg', followed: false, fullName: "Den K.", status: "hey hey hey", location:{country: 'Ukraine', city: 'Lviv'}},
-            {id: 3, photoUrl: 'https://img.freepik.com/free-icon/user_318-159711.jpg', followed: true, fullName: "Maks K.", status: "i am ukrainian too", location:{country: 'Ukraine', city: 'Kiev'}}
-        ])
+
+    let getUsers = () => {
+        if(props.users.length === 0){
+            axios
+                .get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    props.setUsers(response.data.items)
+                })
+            // props.setUsers([
+            //     {id: 1, photoUrl: 'https://img.freepik.com/free-icon/user_318-159711.jpg', followed: true, fullName: "Vlad K.", status: "i am ukrainian", location:{country: 'Ukraine', city: 'Lutsk'}},
+            //     {id: 2, photoUrl: 'https://img.freepik.com/free-icon/user_318-159711.jpg', followed: false, fullName: "Den K.", status: "hey hey hey", location:{country: 'Ukraine', city: 'Lviv'}},
+            //     {id: 3, photoUrl: 'https://img.freepik.com/free-icon/user_318-159711.jpg', followed: true, fullName: "Maks K.", status: "i am ukrainian too", location:{country: 'Ukraine', city: 'Kiev'}}
+            // ])
+        }
     }
+    
     return <div className={styles.wrapper}>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id} className={styles.userBlock}>
                 <div className={styles.avatarContainer}>
-                    <img src={u.photoUrl} className={styles.userPhoto}/>
+                    <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                 </div>
                 <div className={styles.userDescription}>
                     <div className={styles.mainInfoBlock}>
-                        <div className={styles.fullName}>{u.fullName}</div>
-                        <div>{u.status}</div>
+                        <div className={styles.fullName}>{u.name}</div>
+                        {/* <div>{u.status}</div> */}
                     </div>
                     <div className={styles.locationInfoBlock}>
-                        <div>{u.location.country},</div>
-                        <div>{u.location.city}</div>
+                        {/* <div>{u.location.country},</div>
+                        <div>{u.location.city}</div> */}
                     </div>
                 </div>
                 <div>
