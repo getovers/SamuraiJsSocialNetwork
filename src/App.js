a894bceedf2229a10876df96bd181fa4ba0148ea
Vlad Khotynskiy <vlhotinskiy@gmail.com>
2023-08-21 16:36:53 +0300

    added lazy loading for DialogsContainer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { Routes, Route } from 'react-router-dom'
-import DialogsContainer from './components/Dialogs/DialogsContainer';
+// import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
-import { useEffect } from 'react';
+import { Suspense, lazy, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { initializeApp } from './redux/app-reducer';
 import Preloader from './components/common/Preloader/Preloader';
+
+const DialogsContainer = lazy(() => import("./components/Dialogs/DialogsContainer"))
 
 function App(props) {
 
@@ -26,13 +28,15 @@ function App(props) {
       <HeaderContainer />
       <Navbar />
       <div className='app-wrapper-content'>
-        <Routes>
-          <Route path='/' element={<ProfileContainer />} />
-          <Route path='/profile/:userId?' element={<ProfileContainer />} />
-          <Route path='/dialogs/*' element={<DialogsContainer />} />
-          <Route path='/users' element={<UsersContainer />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
+        <Suspense fallback={<Preloader/>}>
+          <Routes>
+            <Route path='/' element={<ProfileContainer />} />
+            <Route path='/profile/:userId?' element={<ProfileContainer />} />
+            <Route path='/dialogs/*' element={<DialogsContainer />} />
+            <Route path='/users' element={<UsersContainer />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </Suspense>
       </div>
     </div>
   );
